e84d2e5df91e5ccce2bccf053825ec38e97dc1b3
Gursimrandeepsingh Khela <gursimrandeepsinghkhela@Gursimrandeepsinghs-MacBook-Air.local>
2025-02-27 23:42:17 -0500

    Added dependencies and folder structure

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+import ChannelBar from './Components/ChannelBar'
+import ChatBox from './Components/ChatBox'
+
+const Home = () => {
+  return (<>
+    <Sidebar/>
+    <ChannelBar/>
+    <ChatBox/>
+    </>
+  )
+}
+
+export default Home
